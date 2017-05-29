c35886fa6d1bffd7b419586893c899aa9aeb55ea
GoMi-AcerLaptop-win10 <ilalh2owei@163.com>
2017-05-29 18:16:45 +0800

    增加调试模式

--- UP_TestAll/main.c
+++ UP_TestAll/main.c
@@ -1,12 +1,13 @@
 #include "UpLib/UP_System.h"
 
+#ifdef DEBUG_ON
+//then the program is in debug mode
+#endif
+
 int main(void) {
     UP_System_Init();
     UP_delay_ms(100);
 
-    UP_CDS_SetMode(4, CDS_SEVMODE);
-
     while (1) {
-        UP_CDS_SetAngle(4, 100, 512);
     }
 }
